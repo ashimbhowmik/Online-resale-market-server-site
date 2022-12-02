097e175a2eaf85e95f5fff2d3ab9b1223ebd3aee
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-12-02 21:29:48 +0600

    [upsert user]

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
   ObjectId,
 } = require("mongodb");
 
-// const admin = require("firebase-admin");
+const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -13,19 +13,19 @@ const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 
-// //firebase
-// const serviceAccount = require("./sports-capturing-firebase-admin.json");
+//firebase
+const serviceAccount = require("./online-resele-market-firebase-adminsdk-44wmw-ebdf02ca8e.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // middlewares
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fbybecg.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mvrzlyk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -34,24 +34,25 @@ const client = new MongoClient(uri, {
 
 //  token middleware
 
-// async function verifyToken(req, res, next) {
-//   if (req.headers?.authorization?.startsWith("Bearer ")) {
-//     const idToken = req.headers.authorization.split(" ")[1];
-//     try {
-//       const decodedUser = await admin.auth().verifyIdToken(idToken);
-//       req.decodedEmail = decodedUser.email;
-//     } catch {}
-//   }
-//   next();
-// }
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const idToken = req.headers.authorization.split(" ")[1];
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(idToken);
+      req.decodedEmail = decodedUser.email;
+    } catch {}
+  }
+  next();
+}
 
 async function run() {
   try {
-    const database = client.db("bi-cycle-server");
+    const database = client.db("computer-server");
     const allProductsCollection = database.collection("allProducts");
     const allBookingCollection = database.collection("allBooking");
     const allUsersCollection = database.collection("allUsers");
     const allPaymentCollection = database.collection("paymentDetails");
+    const advertiseCollection = database.collection("advertise");
 
     //-------------GET AREA Start-----------------//
 
@@ -61,6 +62,15 @@ async function run() {
       const cursor = allProductsCollection.find(query);
       const allProducts = await cursor.toArray();
       res.send(allProducts);
+    });
+
+    // get advertise api
+
+    app.get("/advertise", async (req, res) => {
+      const query = {};
+      const cursor = advertiseCollection.find(query);
+      const advertise = await cursor.toArray();
+      res.send(advertise);
     });
 
     //get all the booking
@@ -86,6 +96,15 @@ async function run() {
       res.json(user);
     });
 
+    // get all user
+
+    app.get("/allUsers", async (req, res) => {
+      const query = {};
+      const cursor = allUsersCollection.find(query);
+      const allUsers = await cursor.toArray();
+      res.send(allUsers);
+    });
+
     //-------------Post AREA Start-----------------//
 
     // Post a new service
@@ -93,6 +112,55 @@ async function run() {
     app.post("/booking", async (req, res) => {
       const booking = req.body;
       const result = await allBookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.post("/addProduct", async (req, res) => {
+      const product = req.body;
+      const result = await allProductsCollection.insertOne(product);
+      res.send(result);
+    });
+
+    // advertise post
+
+    app.post("/advertise", async (req, res) => {
+      const advertise = req.body;
+      const result = await advertiseCollection.insertOne(advertise);
+      res.send(result);
+    });
+
+    //paymentDetails post
+    app.post("/payment", async (req, res) => {
+      const payment = req.body;
+      const result = await allPaymentCollection.insertOne(payment);
+      res.send(result);
+    });
+
+    // post a new user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await allUsersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // upsert user
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await allUsersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // post a new user
+    app.post("/advertise", async (req, res) => {
+      const ads = req.body;
+      const result = await advertiseCollection.insertOne(ads);
       res.send(result);
     });
   } finally {
